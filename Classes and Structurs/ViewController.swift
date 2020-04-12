5f4b14fa79e27c3fa2cd5360b70a7d23b2ba7dbe
nAtaman <ataman@macpaw.com>
2020-04-12 17:11:13 +0300

    Initial Commit

---
+++ Classes and Structurs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Classes and Structurs
+//
+//  Created by Ataman on 12.04.2020.
+//  Copyright © 2020 Ataman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
